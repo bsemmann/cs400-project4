4b271679c5367e90ec795e915b3c7d6717a5bee5
bsemmann <71895133+bsemmann@users.noreply.github.com>
2020-12-08 09:11:10 -0600

    Add files via upload

---
+++ bsemmann_Back_End_Developer_2/TestApplication.java
@@ -0,0 +1,102 @@
+// --== CS400 File Header Information ==--
+// Name: Sriram Alla
+// Email: salla@wisc.edu
+// Team: Team FF
+// Role: Test Engineer
+// TA: Abhay Kumar
+// Lecturer: Gary Dahl
+// Notes to Grader: N/A
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+
+class TestApplication {
+
+  ElectionBackEnd obj = new ElectionBackEnd(); // Object of BackEnd class
+  HashTableMap<String, ArrayList<String>> map = new HashTableMap<>(); // HashTableMap containing all
+                                                                      // the States and their
+                                                                      // election results
+  ArrayList<String> list = new ArrayList<>(); // ArrayList to store the election result of a
+                                              // particular state
+
+  @Test
+  /**
+   * Test method to test the getWinnerName() method.
+   * 
+   */
+
+  void testWinnerName() {
+    list.add("9");
+    list.add("Donald Trump");
+    list.add("62.23");
+    list.add("36.68");
+    map.put("Alabama", list);
+    if (!(obj.getWinnerName("Alabama").equals("Donald Trump")))
+      fail("Not yet implemented");
+  }
+
+  @Test
+  /**
+   * Test method to test the getElectoralVotes() method.
+   * 
+   */
+
+  void testElectoralVotes() {
+    list.add("11");
+    list.add("Joe Biden");
+    list.add("49.39");
+    list.add("49.09");
+    map.put("Arizona", list);
+    if (!(obj.getElectoralVotes("Arizona").equals("11")))
+      fail("Not yet implemented");
+  }
+
+  @Test
+  /**
+   * Test method to test the getWinnerVotePercent() method.
+   * 
+   */
+
+  void testWinnerVotePercent() {
+    list.add("55");
+    list.add("Joe Biden");
+    list.add("63.59");
+    list.add("34.25");
+    map.put("California", list);
+    if (!(obj.getWinnerVotePercent("California").equals("63.59")))
+      fail("Not yet implemented");
+  }
+
+  @Test
+  /**
+   * Test method to test the getLoserVotePercent() method.
+   * 
+   */
+
+  void testLoserVotePercent() {
+    list.add("29");
+    list.add("Donald Trump");
+    list.add("51.22");
+    list.add("47.87");
+    map.put("Florida", list);
+    if (!(obj.getLoserVotePercent("Florida").equals("47.87")))
+      fail("Not yet implemented");
+  }
+
+  @Test
+  /**
+   * Test method to test the getContainsKey() method.
+   * 
+   */
+
+  void testContainsKey() {
+    list.add("9");
+    list.add("Donald Trump");
+    list.add("62.23");
+    list.add("36.68");
+    map.put("Nevada", list);
+    if (!obj.getContainsKey("Nevada"))
+      fail("Not yet implemented");
+  }
+}
